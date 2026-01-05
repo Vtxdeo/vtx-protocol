916e8b84da41e56a8f059c354584b4fa811d8257
Hellohistory <etojsyc521@gmail.com>
2026-01-05 22:17:44 +0800

    feat(protocol): extend WIT and rust bindings

--- packages/rust/src/lib.rs
+++ packages/rust/src/lib.rs
@@ -1,13 +1,15 @@
 use std::path::PathBuf;
 
 /// The raw content of the WIT interface definition.
-pub const WIT_CONTENT: &str = include_str!("../wit/vtx.wit");
+pub const WIT_CONTENT: &str = include_str!("../../../wit/vtx.wit");
 
 /// Returns the absolute path to the bundled vtx.wit file.
 ///
 /// Useful for build scripts or CLI tools that need to reference the protocol definition.
 pub fn get_wit_path() -> PathBuf {
     PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("..")
+        .join("..")
         .join("wit")
         .join("vtx.wit")
 }
